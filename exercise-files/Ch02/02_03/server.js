32fe8873c4fb7ac374d7f8a41ae5faf4739aebf1
jlilly <johannlilly@users.noreply.github.com>
2018-06-27 10:01:21 -0400

    02_03: Use Express to return simple string to requester

--- exercise-files/Ch02/02_03/server.js
+++ exercise-files/Ch02/02_03/server.js
@@ -4,21 +4,44 @@ import express from 'express';
 const server = express();
 
 /**
+ * GET the [root]
+ *
  * Express handles server-side routing for us.
  * Handling routing exposes an API to listen to certain routs.
- * Use `server.get()`
+ * Use `server.get()`.
  * Use `.send` to send a simple string to the requester.
  *
- * @param: the route we are interested in (`root`, aka `\` in this case)
- * @param: the event handler, which receives both a request and response object
- */
+ * @param: the route we are interested in (`root`, aka `\` in this case).
+ * @param: the event handler, which receives both a request and response object.
+ *
+ * @dev: $ babel-node server.js
+ * @dev: $ curl http://localhost:8080/
+ * @dev: # Hello Express
+*/
 server.get('/', (req, res) => {
-	res.send('Hello Express');
+	res.send('Hello Express'); // return simple string
 });
 
 /**
- * @param: the port from configuration file
- * @param: success handler
+ * GET the [About] page.
+ *
+ *
+ * @param: the route we are interested in.
+ * @param: the event handler, which receives both a request and response object
+ *
+ * @dev: $ babel-node server.js
+ * @dev: $ curl http://localhost:8080/about.html
+ * @dev: # The about page
+ */
+server.get('/about.html', (req, res) => {
+	res.send('The about page'); // return simple string
+});
+
+/**
+ * We can use other methods to send things to the requester
+ *
+ * @param: the port from configuration file.
+ * @param: a success handler.
  */
 server.listen(config.port, () => {
 	console.log('Express listening on port ', config.port);
