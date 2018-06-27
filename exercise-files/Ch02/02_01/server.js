4fe0aebddee88f0c70a72f0a5f1f4b487e457c06
jlilly <johannlilly@users.noreply.github.com>
2018-06-27 08:04:07 -0400

    02_01: Supply event listener callback as the argument for `createServer()` itself
    ... and Node will automatically attach the argument to the request event.

--- exercise-files/Ch02/02_01/server.js
+++ exercise-files/Ch02/02_01/server.js
@@ -1,15 +1,25 @@
-import https from 'https'; // core
-// http module for non-ssl
+import http from 'http';
 
-// https.get(...)
-https.get('https://www.lynda.com', res => {
-	// returns string
-	console.log('Response status code: ', res.statusCode);
+const server = http.createServer();
+/// `createServer()` gives event emitter object which we can subscribe to
+/// `request` is the most important emitted event, which gets emitted every time the http server receives a request
 
-	res.on('data', chunk => {
-		console.log(chunk.toString());
-	});
-	
+server.listen(8080);
+
+server.on('request', (req, res) => {
+	res.write('Hello HTTP!\n');
+	setTimeout(() => {
+		res.write('I can stream!\n');
+		res.end();
+	}, 3000); // timeout
 });
+/// the callback for this event receives two (2) special objects: a request object, `req`, and and response object, `res`.
+/// the user initiating the request will see anything we write to the response object
+/// we can use the response object to read things from the request, including:
+/// - what URL was requested
+/// - were there any string parameters?
 
-// $ babel-node server.js | less
+/// the response object is a **"writable string"**, which means we can use it to stream data to the user
+
+/// 	$ babel-node server.js
+///		$ curl http://localhost:8080/
