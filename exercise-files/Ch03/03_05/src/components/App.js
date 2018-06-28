5252ca893926c2d05cdc94836b4054a6592e2f54
jlilly <johannlilly@users.noreply.github.com>
2018-06-28 14:09:25 -0400

    03_05: Component state: stateless function component

--- exercise-files/Ch03/03_05/src/components/App.js
+++ exercise-files/Ch03/03_05/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 
+/// This is referred to as a "stateless" component because we can't use the React state with these components. If a component does not need `state`, this is the best way to define the component.
 const App = () => {
   return (
     <div className="App">
