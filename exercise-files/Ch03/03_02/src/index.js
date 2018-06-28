808a08fc16aecc8260f71c1154778483315045a2
jlilly <johannlilly@users.noreply.github.com>
2018-06-28 10:01:06 -0400

    03_02: React Components - create a component

--- exercise-files/Ch03/03_02/src/index.js
+++ exercise-files/Ch03/03_02/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/// We don't usually work with elements directly, but use componensts.
+/// We are able to create our own JSX-based HTML elements.
+
+const App = () => {
+	return (
+		<h2 className="text-center">
+			Hello React Components!
+		</h2>
+	);
+};
 
 ReactDOM.render(
-  <h2 className="text-center">
-    Hello React with JSX!!
-  </h2>,
+  <App />,
   document.getElementById('root')
 );
