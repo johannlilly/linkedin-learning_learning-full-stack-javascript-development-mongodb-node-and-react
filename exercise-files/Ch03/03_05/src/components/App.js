5483b265ce7424e14c1fe1db8bfe1439b3ac65b3
jlilly <johannlilly@users.noreply.github.com>
2018-06-28 14:13:40 -0400

    03_05: Component state: convert to class-based component, which has state

--- exercise-files/Ch03/03_05/src/components/App.js
+++ exercise-files/Ch03/03_05/src/components/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Header from './Header';
 
-/// This is referred to as a "stateless" component because we can't use the React state with these components. If a component does not need `state`, this is the best way to define the component.
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>
-        ...
-      </div>
-    </div>
-  );
-};
-
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<Header message="Naming Contests" />
+				<div>
+				...
+				</div>
+			</div>
+		);
+	}
+}
 export default App;
