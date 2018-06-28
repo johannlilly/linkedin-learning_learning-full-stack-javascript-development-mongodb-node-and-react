fe71f930dab8072beee9b43886868a4b343c89e9
jlilly <johannlilly@users.noreply.github.com>
2018-06-28 14:21:13 -0400

    03_05: Introduce state into class-based component by using a constructor; declare properties for the state object

--- exercise-files/Ch03/03_05/src/components/App.js
+++ exercise-files/Ch03/03_05/src/components/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Header from './Header';
 
 class App extends React.Component {
+  /// Use constructor to introduce `state`
+  constructor(props) {
+    super(props);
+    /// state object with property `test` on the object
+    this.state = { test: 42 };
+  }
 	render() {
 		return (
 			<div className="App">
 				<Header message="Naming Contests" />
 				<div>
-				...
+          {/* Use property on state object inside a variable */}
+				  {this.state.test}
 				</div>
 			</div>
 		);
