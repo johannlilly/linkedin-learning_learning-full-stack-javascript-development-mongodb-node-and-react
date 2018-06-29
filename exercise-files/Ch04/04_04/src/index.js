fa7a6bebac56d1e93e4011688198e88f6e062b2a
jlilly <johannlilly@users.noreply.github.com>
2018-06-29 17:15:50 -0400

    04_04: Render React application without errors without having data with an emty array

--- exercise-files/Ch04/04_04/src/index.js
+++ exercise-files/Ch04/04_04/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import data from './testData';
 import App from './components/App';
 
+
+/// Before data is loaded, render the react application with an empty array
 ReactDOM.render(
-  <App contests={data.contests} />,
+  <App contests={[]} />,
   document.getElementById('root')
 );
