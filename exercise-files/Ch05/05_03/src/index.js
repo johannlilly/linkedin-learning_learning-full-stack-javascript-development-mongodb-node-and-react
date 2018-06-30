259cee3c2362786d41ca6be3178f8c30c238c7b3
jlilly <johannlilly@users.noreply.github.com>
2018-06-30 13:04:27 -0400

    05_03: Fix the checksum problem by wrapping ReactDOM in `axios`

--- exercise-files/Ch05/05_03/src/index.js
+++ exercise-files/Ch05/05_03/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import App from './components/App';
-
-ReactDOM.render(
-  <App initialContests={[]} />,
-  document.getElementById('root')
-);
+/// markup rendering from the server matches what React starts with
+axios.get('/api/contests')
+  .then(resp => {
+    ReactDOM.render(
+      <App initialContests={resp.data.contests} />, // initialize with same data as on the server
+      document.getElementById('root')
+    );
+    // this.setState({
+    //   contests: resp.data.contests
+    // });
+  })
+  .catch(console.error);
