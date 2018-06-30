f9053fe624664dba0a859ec5da5e678913718151
jlilly <johannlilly@users.noreply.github.com>
2018-06-30 17:21:14 -0400

    06_03: Convert our data into an object

--- exercise-files/Ch06/06_03/api/index.js
+++ exercise-files/Ch06/06_03/api/index.js
@@ -4,7 +4,13 @@ import data from '../src/testData';
 const router = express.Router();
 
 router.get('/contests', (req, res) => {
-  res.send({ contests: data.contests });
+  res.send({
+    // convert to object
+    contests: data.contests.reduce((obj, contest) => { // expose object we will return; return `contest` object with every iteration
+      obj[contest.id] = contest; // assign as a property on the object with the `id` being the `contest.id`
+      return obj; // values are the actual contest objects
+    }, {}) // start with an empty object
+  });
 });
 
 export default router;
