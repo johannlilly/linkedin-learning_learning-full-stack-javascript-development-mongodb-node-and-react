f8bf0a93fa5d183f84903467aa79ee21507015e6
jlilly <johannlilly@users.noreply.github.com>
2018-06-30 08:34:10 -0400

    04_05: Fetching data from a remote API after data loaded "in `state`" using an AJAX request instead of working with the data directly from memory

--- exercise-files/Ch04/04_05/src/components/App.js
+++ exercise-files/Ch04/04_05/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 import data from '../testData';
@@ -9,9 +10,32 @@ class App extends React.Component {
     contests: []
   };
   componentDidMount() {
-    this.setState({
-      contests: data.contests
-    });
+    /**
+     * AJAX request: `.get()`.
+     * axios is promise-based.
+     * Handle every promise with `.then()`.
+     * Catch every error with `.catch()`.
+     *
+     * @param URL of API endpoint to read
+     *
+    */
+    axios.get('/api/contests')
+      /**
+       * axios `.then()` method
+       * 
+       * returns a response object `resp` with the data
+      */
+      .then(resp => {
+        /**
+         * Set the state according to and directly from the response data we are getting back through axios.
+         *
+        */
+        // console.log(resp.data.contests);
+        this.setState({
+          contests: resp.data.contests
+        });
+      })
+      .catch(console.error)
   }
   componentWillUnmount() {
     // clean timers, listeners
