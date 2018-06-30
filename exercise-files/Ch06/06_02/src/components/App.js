0da99993bc986af1cb1b9b17d9a4f68976385e19
jlilly <johannlilly@users.noreply.github.com>
2018-06-30 16:52:47 -0400

    06_02: Navigating to a contest with basic HTML 5 routing

--- exercise-files/Ch06/06_02/src/components/App.js
+++ exercise-files/Ch06/06_02/src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
 
+const pushState = (obj, url) => 
+  window.history.pushState(obj, '', url);
+
 class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests',
@@ -13,11 +16,20 @@ class App extends React.Component {
   componentWillUnmount() {
     // clean timers, listeners
   }
+  // fetch contest information from server
+  fetchContest = (contestId) => {
+    pushState(
+      { currentContestId: contestId }, // object about that record; a state object
+      `/contests/${contestId}` // url
+    );
+  };
   render() {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
-        <ContestList contests={this.state.contests} />
+        <ContestList 
+          onContestClick={this.fetchContest}
+          contests={this.state.contests} />
       </div>
     );
   }
